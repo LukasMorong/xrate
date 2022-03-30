76016496dc841f829fa512914a0cc95609e89ea6
LukasMorong <morong.lukas@gmail.com>
2022-03-30 22:51:10 +0200

    add header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/layout/Navbar';
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/layout/Navbar.js
@@ -0,0 +1,13 @@
+export default function Example() {
+  return (
+    <nav className="bg-white border-gray-200 px-2 sm:px-4 py-4 rounded-none drop-shadow-md dark:bg-gray-800">
+      <div className="container flex flex-wrap justify-between items-center mx-auto">
+        <p className="flex items-center">
+          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+            XRate
+          </span>
+        </p>
+      </div>
+    </nav>
+  );
+}
