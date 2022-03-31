f6e533ab4e0ca2110b57fee16314cd1760b18479
LukasMorong <morong.lukas@gmail.com>
2022-03-31 23:13:10 +0200

    add currency value input

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,42 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+//components
 import Navbar from './components/layout/Navbar';
+import ValueInput from './components/ValueInput';
 
 function App() {
+  const [rates, setRates] = useState([]);
+  const [curr1Amount, setCurr1Amount] = useState(1);
+  const [curr2Amount, setCurr2Amount] = useState(1);
+  const [currency1, setCurency1] = useState('USD');
+  const [currency2, setCurency2] = useState('EUR');
+
+  useEffect(() => {
+    axios
+      .get('http://data.fixer.io/api/latest?access_key=24ad502032d75aef57c648f1586dd230')
+      .then((res) => {
+        setRates(res.data.rates);
+      });
+  }, []);
+
   return (
     <div className="App">
       <Navbar />
+      <ValueInput
+        onAmountChange={setCurr1Amount}
+        onCurrencyChange={setCurency1}
+        currencies={Object.keys(rates)}
+        amount={curr1Amount}
+        currency={currency1}
+      />
+      <ValueInput
+        onAmountChange={setCurr2Amount}
+        onCurrencyChange={setCurency2}
+        currencies={Object.keys(rates)}
+        amount={curr2Amount}
+        currency={currency2}
+      />
     </div>
   );
 }
